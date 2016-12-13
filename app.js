c4121a37dd4195d24129df072ce8347996ebe4bf
Yash <yashpatel256@gmail.com>
2016-12-13 00:15:43 -0500

    Added functionalities of delete and modal confirmatation using vanilla javascript

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
  */
     'use strict';
 
+
+    var modal = document.getElementById("myModal");
+    var modal2 = document.getElementById("deleteModal");
+    var span = document.getElementById("sendModal");
+    var del_modal = document.getElementById("deleteMod");
+    var okBtn = document.getElementById("okBtn");
+
     function overlay() {
         var el = document.getElementById("overlay");
         el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
@@ -13,9 +20,19 @@
         var mail = document.getElementById("txt").value;
         document.getElementById("overlay").style.visibility = "hidden";
 
+        modal.style.display = "block";
+        modal.style.overflow = "inherit";
+        span.onclick = function () {
+          modal.style.display = "none";
+        };
+        okBtn.onclick = function () {
+          modal.style.display = "none";
+        };
         var parent = document.getElementById("sentmail");
-        var anchor = document.createElement("a");
-        anchor.className = "anchorTag";
+        // var anchor = document.createElement("a");
+        // anchor.className = "anchorTag";
+        var divs = document.createElement("div");
+        divs.className = "inbox_container";
         var input = document.createElement("input");
         input.type = "checkbox";
         var sp1 = document.createElement("span");
@@ -31,13 +48,12 @@
         var message = document.createTextNode(mail);
         sp3.appendChild(message);
         var br = document.createElement("br");
-        anchor.appendChild(input);
-        anchor.appendChild(sp1);
-        anchor.appendChild(sp2);
-        anchor.appendChild(sp3);
-        parent.appendChild(anchor);
-        parent.appendChild(br);
-        openSent();
+        divs.appendChild(input);
+        divs.appendChild(sp1);
+        divs.appendChild(sp2);
+        divs.appendChild(sp3);
+        parent.appendChild(divs);
+        console.log(parent);
     }
     function openSent() {
         var parent = document.getElementById("sentmail");
@@ -45,13 +61,58 @@
         var elem = document.getElementById("tab");
         elem.style.display = "none";
         document.getElementById("inbox").className = "button2 font-8";
+        document.getElementById("trashBtn").className = "button2 font-8";
         document.getElementById("sentBtn").className += " active";
     }
     function openInbox() {
         var parent = document.getElementById("sentmail");
         parent.style.display = "none";
+        var trash = document.getElementById("trashmail");
+        trash.style.display = "none";
         var elem = document.getElementById("tab");
         elem.style.display = "inherit";
         document.getElementById("inbox").className += " active";
         document.getElementById("sentBtn").className = "button2 font-8";
+        document.getElementById("trashBtn").className = "button2 font-8";
+    }
+    
+    function deleteMails() {
+        var checked = document.querySelectorAll('input:checked');
+        var arr = [];
+        for (var i = 0; i < checked.length; i++) {
+            arr.push(checked[i].parentElement);
+            checked[i].parentElement.remove();
+        }
+        var trash = document.getElementById("trashmail");
+        for (var i = 0; i < arr.length; i++) {
+            trash.appendChild(arr[i]);
+        }
+        // console.log(trash);
+    }
+    
+    function openTrash() {
+        var parent = document.getElementById("trashmail");
+        parent.style.display = "inherit";
+        var elem = document.getElementById("tab");
+        elem.style.display = "none";
+        var sent = document.getElementById("sentmail");
+        sent.style.display = "none";
+        document.getElementById("inbox").className = "button2 font-8";
+        document.getElementById("sentBtn").className = "button2 font-8";
+        document.getElementById("trashBtn").className += " active";
+    }
+    function modalDelete() {
+        modal2.style.display = "block";
+        modal2.style.overflow = "inherit";
+        document.getElementById("cancelBtn").onclick = function () {
+            modal2.style.display = "none";
+        }
+        del_modal.onclick = function () {
+            modal2.style.display = "none";
+        }
+    }
+
+    function cont() {
+        modal2.style.display = "none";
+        deleteMails();
     }
